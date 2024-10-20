37c384dadd0004b99bee53f57200812e34ba18e1
diego <creacioneszaide@gmail.com>
2024-10-20 17:07:51 -0500

    convertir los valores ingresados

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let base = prompt("ingresa la base");
 let exponente = prompt("ingresa el exponente");
 
-
+// convertir los valores ingresado a numeros
+base = parseFloat(base);
+exponente = parseFloat(exponente);
 
 
