5c0442a89acb20d25aa65e6bc926a6f1ab63327d
diego <creacioneszaide@gmail.com>
2024-10-20 17:06:09 -0500

    inicio del cuarto ejercicio

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-/pedir al usuario que ingrese la base y la altura del triangulo//
-let base = prompt("ingrese la base del triangulo");
-let altura = prompt("ingrese altura");
+// solicitar la base y el exponer al usuario
+let base = prompt("ingresa la base");
+let exponente = prompt("ingresa el exponente");
 
-//convertir los valores en numero
-base = parseFloat(base);
-altura = parseFloat(altura);
 
-//calcular base de triangulo (formula: base * altura / 2)
-let area = (base * altura)/ 2;
 
-//mostrar el resultado
-document.write ("el area del triangulo es " + area.toFixed(2));
+
